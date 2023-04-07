74f151d63fdf40cd08e7526d4b7c6c298e5da5a1
Black1358 <122863429+Black1358@users.noreply.github.com>
2023-04-07 04:25:20 +0300

    Add files via upload

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	plugins: {
+		'postcss-import': {},
+		'postcss-simple-vars': {},
+		'postcss-minify': {},
+		'tailwindcss/nesting': {},
+		tailwindcss: {},
+		autoprefixer: {},
+	}
+}
